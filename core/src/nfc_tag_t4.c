5644dacb03d177553510ce296f4965268a330b0b
Slava Monich <slava.monich@jolla.com>
2020-02-26 16:33:48 +0200

    [core] Implemented reading NDEF from a Type 4 tag. Fixes JB#49087

--- core/src/nfc_tag_t4.c
+++ core/src/nfc_tag_t4.c
@@ -35,6 +35,8 @@
 #include "nfc_tag_t4_p.h"
 #include "nfc_tag_p.h"
 #include "nfc_target_p.h"
+#include "nfc_ndef.h"
+#include "nfc_util.h"
 #include "nfc_log.h"
 
 typedef struct nfc_isodep_tx {
@@ -44,13 +46,37 @@ typedef struct nfc_isodep_tx {
     void* user_data;
 } NfcIsoDepTx;
 
+typedef struct nfc_iso_dep_ndef_read {
+    NfcTagType4* t4;
+    guint8 fid[2];
+    guint data_len;
+    guint max_read;
+    GByteArray* data;
+} NfcIsoDepNdefRead;
+
 struct nfc_tag_t4_priv {
     guint mtu;  /* FSC (Type 4A) or FSD (Type 4B) */
     GByteArray* buf;
+    NfcTargetSequence* init_seq;
+    NfcIsoDepNdefRead* init_read;
     guint init_id;
 };
 
 G_DEFINE_ABSTRACT_TYPE(NfcTagType4, nfc_tag_t4, NFC_TYPE_TAG)
+
+/*
+ * NFCForum-TS-Type-4-Tag_2.0
+ * Section 5.4.2. NDEF Tag Application Select Procedure
+ */
+
+static const guint8 ndef_aid[] = { 0xD2, 0x76, 0x00, 0x00, 0x85, 0x01, 0x01 };
+static const guint8 ndef_cc_ef[] = { 0xE1, 0x03 };
+static const GUtilData ndef_aid_data = { ndef_aid, sizeof(ndef_aid) };
+static const GUtilData ndef_cc_ef_data = { ndef_cc_ef, sizeof(ndef_cc_ef) };
+
+#define ISO_SW_NDEF_NOT_FOUND (0x6a82)
+#define NDEF_CC_LEN (15)
+#define NDEF_DATA_OFFSET (2)
 
 /* ISO/IEC 7816-4 */
 
@@ -60,7 +86,9 @@ G_DEFINE_ABSTRACT_TYPE(NfcTagType4, nfc_tag_t4, NFC_TYPE_TAG)
 
 #define ISO_SHORT_FID_MASK (0x1f) /* Short File ID mask */
 
+/* Instruction byte */
 #define ISO_INS_SELECT (0xA4)
+#define ISO_INS_READ_BINARY (0xB0)
 
 /* Selection by file identifier */
 #define ISO_P1_SELECT_BY_ID (0x00)      /* Select MF, DF or EF */
@@ -263,6 +291,378 @@ nfc_isodep_submit(
     return 0;
 }
 
+static
+NfcIsoDepNdefRead*
+nfc_iso_dep_ndef_read_new(
+    NfcTagType4* self,
+    const guint8* cc /* At least 15 bytes */)
+{
+    /* See Table 4: Data Structure of the Capability Container File */
+    /* And Section 5.1.2.1 NDEF File Control TLV */
+    if ((cc[2] >> 4) == 2 /* We expect Version 2 of the spec */ &&
+        cc[7] == 4 && cc[8] == 6 /* File Control TLV, T = 4, L = 6 */) {
+        const guint8* v = cc + 9; /* V part of File Control TLV */
+
+        /* Check NDEF file read access condition */
+        if (v[4] == 0 /* read access granted */) {
+            /*
+             * 5.1.2.1 NDEF File Control TLV
+             *
+             * ...
+             * [RQ_T4T_NDA_016] File Identifier, 2 bytes. Indicates a valid
+             * NDEF file. The valid ranges are 0001h to E101h, E104h to 3EFFh,
+             * 3F01h to 3FFEh and 4000h to FFFEh. The values 0000h, E102h,
+             * E103h, 3F00h and 3FFFh are reserved (see [ISO/IEC_7816-4])
+             * and FFFFh is RFU.
+             */
+            const guint fid = ((((guint)(v[0])) << 8) | v[1]);
+
+            if ((fid >= 0x0001 && fid <= 0xE101) ||
+                (fid >= 0xE104 && fid <= 0x3EFF) ||
+                (fid >= 0x3F01 && fid <= 0x3FFE) ||
+                (fid >= 0x4000 && fid <= 0xFFFE)) {
+                const guint max_read = ((((guint)(cc[3])) << 8) | cc[4]);
+
+                /* The valid values for MLe are 000Fh-FFFFh */
+                if (max_read >= 0x000f) {
+                    NfcIsoDepNdefRead* read = g_slice_new0(NfcIsoDepNdefRead);
+
+                    read->max_read = max_read;
+                    read->fid[0] = v[0];
+                    read->fid[1] = v[1];
+                    read->t4 = self;
+                    GDEBUG("NDEF file: %04X", fid);
+                    GVERBOSE("Max read: %u bytes", read->max_read);
+                    return read;
+                } else {
+                    GDEBUG("MLe too small (%u)", max_read);
+                }
+            } else {
+                GDEBUG("Invalid NDEF file id %04X", fid);
+            }
+        } else {
+            GDEBUG("NDEF read not allowed");
+        }
+    } else {
+        GDEBUG("Unexpected structure of NDEF Capability Container");
+    }
+    return NULL;
+}
+
+static
+void
+nfc_iso_dep_ndef_read_free(
+    NfcIsoDepNdefRead* read)
+{
+    if (read) {
+        if (read->data) {
+            g_byte_array_free(read->data, TRUE);
+        }
+        g_slice_free1(sizeof(*read), read);
+    }
+}
+
+static
+void
+nfc_tag_t4_initialized(
+    NfcTagType4* self)
+{
+    NfcTagType4Priv* priv = self->priv;
+    NfcTag* tag = &self->tag;
+
+    GASSERT(!priv->init_id);
+    nfc_target_sequence_unref(priv->init_seq);
+    nfc_iso_dep_ndef_read_free(priv->init_read);
+    priv->init_seq = NULL;
+    priv->init_read = NULL;
+    nfc_tag_set_initialized(tag);
+}
+
+static
+void
+nfc_tag_t4_init_done(
+    NfcTarget* target,
+    void* user_data)
+{
+    nfc_tag_t4_initialized(NFC_TAG_T4(user_data));
+}
+
+static
+void
+nfc_tag_t4_ndef_read_done(
+    NfcTagType4* self)
+{
+    NfcTagType4Priv* priv = self->priv;
+
+    /*
+     * The initialization sequence has completed, release it. It must be
+     * done now, to avoid blocking presence checks in case if reactivation
+     * times out.
+     */
+    nfc_target_sequence_unref(priv->init_seq);
+    priv->init_seq = NULL;
+    GDEBUG("Reactivating Type 4 tag");
+    if (!nfc_target_reactivate(self->tag.target, nfc_tag_t4_init_done, self)) {
+        GDEBUG("Oops. Failed to reactivate, leaving the tag as is");
+        nfc_tag_t4_initialized(self);
+    }
+}
+
+static
+guint
+nfc_isodep_init_read_binary(
+    NfcTagType4* self,
+    guint offset,
+    guint le,
+    NfcTagType4ResponseFunc resp)
+{
+    return nfc_isodep_submit(self, ISO_CLA, ISO_INS_READ_BINARY,
+        (guint8)(offset >> 8), (guint8)offset, NULL, le,
+        self->priv->init_seq, resp, NULL, NULL);
+}
+
+static
+void
+nfc_tag_t4_init_read_ndef_data_resp(
+    NfcTagType4* self,
+    guint sw,
+    const void* data,
+    guint len,
+    void* user_data)
+{
+    NfcTagType4Priv* priv = self->priv;
+
+    if (sw == ISO_SW_OK) {
+        if (len > 0) {
+            NfcIsoDepNdefRead* read = priv->init_read;
+            GByteArray* buf = read->data;
+
+            g_byte_array_append(buf, data, len);
+            if (buf->len < read->data_len) {
+                const guint remaining = read->data_len - buf->len;
+
+                if ((priv->init_id = nfc_isodep_init_read_binary(self,
+                    buf->len + NDEF_DATA_OFFSET,
+                    MIN(remaining, read->max_read),
+                    nfc_tag_t4_init_read_ndef_data_resp)) != 0) {
+                    return;
+                }
+            } else {
+                GUtilData ndef;
+
+                /* Parse the NDEF */
+                ndef.bytes = buf->data;
+                ndef.size = buf->len;
+                self->tag.ndef = nfc_ndef_rec_new(&ndef);
+            }
+        } else {
+            GDEBUG("Empty NDEF read");
+        }
+    } else if (sw != ISO_SW_IO_ERR) {
+        GDEBUG("NDEF read error %04X", sw);
+    } else {
+        GDEBUG("NDEF read I/O error");
+    }
+    priv->init_id = 0;
+    nfc_tag_t4_ndef_read_done(self);
+}
+
+static
+void
+nfc_tag_t4_init_read_ndef_len_resp(
+    NfcTagType4* self,
+    guint sw,
+    const void* data,
+    guint len,
+    void* user_data)
+{
+    NfcTagType4Priv* priv = self->priv;
+
+    if (sw == ISO_SW_OK) {
+        if (len == NDEF_DATA_OFFSET) {
+            NfcIsoDepNdefRead* read = priv->init_read;
+            const guint8* bytes = data;
+
+            read->data_len = ((((guint)(bytes[0])) << 8) | bytes[1]);
+            if (read->data_len > 0) {
+                GDEBUG("Reading %u bytes of NDEF data", read->data_len);
+                read->data = g_byte_array_sized_new(read->data_len);
+                if ((priv->init_id = nfc_isodep_init_read_binary(self,
+                    NDEF_DATA_OFFSET, MIN(read->data_len, read->max_read),
+                    nfc_tag_t4_init_read_ndef_data_resp)) != 0) {
+                    return;
+                }
+            } else {
+                GDEBUG("NDEF is empty");
+            }
+        } else {
+            GDEBUG("Unexpected number of bytes from NDEF file (%u)", len);
+        }
+    } else if (sw != ISO_SW_IO_ERR) {
+        GDEBUG("NDEF read error %04X", sw);
+    } else {
+        GDEBUG("NDEF read I/O error");
+    }
+    priv->init_id = 0;
+    nfc_tag_t4_ndef_read_done(self);
+}
+
+static
+void
+nfc_tag_t4_init_select_ndef_resp(
+    NfcTagType4* self,
+    guint sw,
+    const void* data,
+    guint len,
+    void* user_data)
+{
+    NfcTagType4Priv* priv = self->priv;
+
+    if (sw == ISO_SW_OK) {
+        NfcIsoDepNdefRead* read = priv->init_read;
+
+        GDEBUG("Selected %02X%02X", read->fid[0], read->fid[1]);
+        /* Read first 2 bytes of the NDEF file (record size) */
+        if ((priv->init_id = nfc_isodep_init_read_binary(self, 0,
+            NDEF_DATA_OFFSET, nfc_tag_t4_init_read_ndef_len_resp)) != 0) {
+            return;
+        }
+    } else if (sw != ISO_SW_IO_ERR) {
+        GDEBUG("NDEF file selection error %04X", sw);
+    } else {
+        GDEBUG("NDEF file selection I/O error");
+    }
+    priv->init_id = 0;
+    nfc_tag_t4_ndef_read_done(self);
+}
+
+static
+void
+nfc_tag_t4_init_read_ndef_cc_resp(
+    NfcTagType4* self,
+    guint sw,
+    const void* data,
+    guint len,
+    void* user_data)
+{
+    NfcTagType4Priv* priv = self->priv;
+
+    if (sw == ISO_SW_OK) {
+        if (len < NDEF_CC_LEN) {
+            GDEBUG("Not enough data for NDEF Capability Container");
+        } else {
+            GDEBUG("NDEF Capability Container");
+            nfc_hexdump(data, len);
+            priv->init_read = nfc_iso_dep_ndef_read_new(self, data);
+            if (priv->init_read) {
+                GUtilData fid;
+
+                /*
+                 * Table 18: NDEF Select Command C-APDU
+                 * 00A4000C02xxxx
+                 */
+                fid.bytes = priv->init_read->fid;
+                fid.size = 2;
+                if ((priv->init_id = nfc_isodep_submit(self, ISO_CLA,
+                    ISO_INS_SELECT, ISO_P1_SELECT_BY_ID,
+                    ISO_P2_SELECT_FILE_FIRST | ISO_P2_RESPONSE_NONE,
+                    &fid, 0, priv->init_seq,
+                    nfc_tag_t4_init_select_ndef_resp, NULL, NULL)) != 0) {
+                    return;
+                }
+            }
+        }
+    } else if (sw != ISO_SW_IO_ERR) {
+        GDEBUG("NDEF Capability Container read error %04X", sw);
+    } else {
+        GDEBUG("NDEF Capability Container read I/O error");
+    }
+    priv->init_id = 0;
+    nfc_tag_t4_ndef_read_done(self);
+}
+
+static
+void
+nfc_tag_t4_init_select_ndef_cc_resp(
+    NfcTagType4* self,
+    guint sw,
+    const void* data,
+    guint len,
+    void* user_data)
+{
+    NfcTagType4Priv* priv = self->priv;
+
+    /*
+     * NFCForum-TS-Type-4-Tag_2.0
+     * Table 14: Capability Container Select Command -
+     *           Detailed R-APDU Field Description
+     *
+     * 90h 00h   Command completed
+     * 6Ah 82h   Capability container not found
+     */
+    if (sw == ISO_SW_OK) {
+        GVERBOSE("NDEF Capability Container selected");
+        /* Read first 15 bytes of CC */
+        if ((priv->init_id = nfc_isodep_init_read_binary(self, 0, NDEF_CC_LEN,
+            nfc_tag_t4_init_read_ndef_cc_resp)) != 0) {
+            return;
+        }
+    } else if (sw == ISO_SW_NDEF_NOT_FOUND) {
+        GDEBUG("NDEF Capability Container not found");
+    } else if (sw != ISO_SW_IO_ERR) {
+        GDEBUG("NDEF Capability Container selection error %04X", sw);
+    } else {
+        GDEBUG("NDEF Capability Container selection I/O error");
+    }
+    priv->init_id = 0;
+    nfc_tag_t4_ndef_read_done(self);
+}
+
+static
+void
+nfc_tag_t4_init_select_ndef_app_resp(
+    NfcTagType4* self,
+    guint sw,
+    const void* data,
+    guint len,
+    void* user_data)
+{
+    NfcTagType4Priv* priv = self->priv;
+
+    /*
+     * NFCForum-TS-Type-4-Tag_2.0
+     * Table 11: NDEF Tag Application Select -
+     *           Detailed R-APDU Field Description
+     *
+     * 90h 00h   Command completed
+     * 6Ah 82h   NDEF Tag Application not found
+     */
+    if (sw == ISO_SW_OK) {
+        GDEBUG("Found NDEF Tag Application");
+        /*
+         * Table 12: Capability Container Select Command C-APDU
+         * 00A4000C02E103
+         */
+        if ((priv->init_id = nfc_isodep_submit(self, ISO_CLA,
+            ISO_INS_SELECT, ISO_P1_SELECT_BY_ID,
+            ISO_P2_SELECT_FILE_FIRST | ISO_P2_RESPONSE_NONE,
+            &ndef_cc_ef_data, 0, priv->init_seq,
+            nfc_tag_t4_init_select_ndef_cc_resp, NULL, NULL)) != 0) {
+            return;
+        }
+    } else if (sw == ISO_SW_NDEF_NOT_FOUND) {
+        GDEBUG("NDEF Tag Application not found");
+    } else if (sw != ISO_SW_IO_ERR) {
+        GDEBUG("NDEF Tag Application selection error %04X", sw);
+    } else {
+        GDEBUG("NDEF Tag Application selection I/O error");
+    }
+
+    /* No need to reinitialize the tag in this case */
+    priv->init_id = 0;
+    nfc_tag_t4_initialized(self);
+}
+
 /*==========================================================================*
  * Internal interface
  *==========================================================================*/
@@ -273,22 +673,43 @@ nfc_tag_t4_init_base(
     NfcTarget* target,
     guint mtu)
 {
+    NfcTag* tag = &self->tag;
     NfcTagType4Priv* priv = self->priv;
 
-    nfc_tag_init_base(&self->tag, target);
+    nfc_tag_init_base(tag, target);
     priv->mtu = mtu;
 
     /*
-     * For more information in NDEF application and fetching NDEF
-     * from a Type 4 tag, see NFCForum-TS-Type-4-Tag_2.0 section
-     * 5.1 NDEF Management.
+     * We only try to read NDEF Tag file if the target can be reactivated.
+     * Reactivation is supported by most commonly used NCI-based adapter
+     * implementations.
      *
-     * Better leave the default application selected and ignore this
-     * NDEF stuff for now until this feature is requested and even more
-     * importantly we have a real card to test against.
+     * Reactivation is required in order to reset the card back to its
+     * pristine initial state (with default application selected). Since
+     * we have no reliable way to determine the default application, the
+     * most reliable (and perhaps the only) way to re-select the default
+     * application is to reinitialize the card from scratch. Besides,
+     * selection of a non-default application may be an irreversible
+     * action (which of course depends on how the card is programmed).
      */
-#pragma message("TODO: Read NDEF")
-    nfc_tag_set_initialized(&self->tag);
+    if (nfc_target_can_reactivate(tag->target)) {
+        priv->init_seq = nfc_target_sequence_new(target);
+
+        /*
+         * NFCForum-TS-Type-4-Tag_2.0
+         * Section 5.4.2. NDEF Tag Application Select Procedure
+         *
+         * Table 9: NDEF Tag Application Select C-APDU
+         * 00A4040007D276000085010100
+         */
+        if ((priv->init_id = nfc_isodep_submit(self, ISO_CLA, ISO_INS_SELECT,
+            ISO_P1_SELECT_DF_BY_NAME, ISO_P2_SELECT_FILE_FIRST, &ndef_aid_data,
+            0x100, priv->init_seq, nfc_tag_t4_init_select_ndef_app_resp,
+            NULL, NULL)) != 0) {
+            return;
+        }
+    }
+    nfc_tag_t4_initialized(self);
 }
 
 /*==========================================================================*
@@ -338,6 +759,8 @@ nfc_tag_t4_finalize(
     NfcTagType4Priv* priv = self->priv;
 
     nfc_target_cancel_transmit(self->tag.target, priv->init_id);
+    nfc_target_sequence_unref(priv->init_seq);
+    nfc_iso_dep_ndef_read_free(priv->init_read);
     g_byte_array_free(priv->buf, TRUE);
     G_OBJECT_CLASS(nfc_tag_t4_parent_class)->finalize(object);
 }
