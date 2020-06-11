a833403afbeda5d24f596399b5a947b34f1ddd87
Aleksei Berman <sr.Lux1nt@gmail.com>
2020-06-11 15:20:02 +0000

    [dbus] Add GetActivationParameters method for IsoDep interface. JB#50041
    It's possible now to retrieve Activation parameters for ISO-DEP
    interface tags.

--- plugins/dbus_service/dbus_service_isodep.c
+++ plugins/dbus_service/dbus_service_isodep.c
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2019 Jolla Ltd.
  * Copyright (C) 2019 Slava Monich <slava.monich@jolla.com>
+ * Copyright (C) 2020 Open Mobile Platform LLC.
  *
  * You may use this file under the terms of BSD license as follows:
  *
@@ -34,6 +35,7 @@
 #include "dbus_service/org.sailfishos.nfc.IsoDep.h"
 
 #include <nfc_tag_t4.h>
+#include <nfc_target.h>
 
 #include <gutil_misc.h>
 
@@ -41,6 +43,8 @@ enum {
     CALL_GET_ALL,
     CALL_GET_INTERFACE_VERSION,
     CALL_TRANSMIT,
+    CALL_GET_ALL2,
+    CALL_GET_ACTIVATION_PARAMETERS,
     CALL_COUNT
 };
 
@@ -51,7 +55,7 @@ struct dbus_service_isodep {
     gulong call_id[CALL_COUNT];
 };
 
-#define NFC_DBUS_ISODEP_INTERFACE_VERSION  (1)
+#define NFC_DBUS_ISODEP_INTERFACE_VERSION  (2)
 
 typedef struct dbus_service_isodep_async_call {
     OrgSailfishosNfcIsoDep* iface;
@@ -68,6 +72,38 @@ dbus_service_isodep_dup_data_as_variant(
         g_variant_new_fixed_array(G_VARIANT_TYPE_BYTE, data, size, 1) :
         g_variant_new_from_data(G_VARIANT_TYPE("ay"), NULL, 0, TRUE,
             NULL, NULL);
+}
+
+static
+void
+dbus_service_isodep_dict_add_value(
+    GVariantBuilder* builder,
+    const char* name,
+    GVariant* value)
+{
+    g_variant_builder_add(builder, "{sv}", name, value);
+}
+
+static
+void
+dbus_service_isodep_dict_add_byte(
+    GVariantBuilder* builder,
+    const char* name,
+    guint8 value)
+{
+    dbus_service_isodep_dict_add_value(builder, name,
+        g_variant_new_byte(value));
+}
+
+static
+void
+dbus_service_isodep_dict_add_bytes_array(
+    GVariantBuilder* builder,
+    const char* name,
+    const GUtilData* data)
+{
+    dbus_service_isodep_dict_add_value(builder, name,
+        dbus_service_isodep_dup_data_as_variant(data->bytes, data->size));
 }
 
 static
@@ -206,6 +242,83 @@ dbus_service_isodep_handle_transmit(
     return TRUE;
 }
 
+/* Interface version 2 */
+
+static
+GVariant*
+dbus_service_isodep_get_act_parameters(
+    NfcTagType4* t4,
+    const NfcParamIsoDep* act)
+{
+    GVariantBuilder gvb;
+    g_variant_builder_init(&gvb, G_VARIANT_TYPE("a{sv}"));
+
+    if (G_LIKELY(act)) {
+        switch(t4->tag.target->technology) {
+        case NFC_TECHNOLOGY_A:
+            dbus_service_isodep_dict_add_byte(&gvb, "T0", act->a.t0);
+            if (act->a.t0 & NFC_PARAM_ISODEP_T0_A) {
+                dbus_service_isodep_dict_add_byte(&gvb, "TA", act->a.ta);
+            }
+            if (act->a.t0 & NFC_PARAM_ISODEP_T0_B) {
+                dbus_service_isodep_dict_add_byte(&gvb, "TB", act->a.tb);
+            }
+            if (act->a.t0 & NFC_PARAM_ISODEP_T0_C) {
+                dbus_service_isodep_dict_add_byte(&gvb, "TC", act->a.tc);
+            }
+            dbus_service_isodep_dict_add_bytes_array(&gvb, "HB", &act->a.t1);
+            break;
+        case NFC_TECHNOLOGY_B:
+            dbus_service_isodep_dict_add_byte(&gvb, "MBLI", act->b.mbli);
+            dbus_service_isodep_dict_add_byte(&gvb, "DID", act->b.did);
+            if (act->b.hlr.bytes) {
+                dbus_service_isodep_dict_add_bytes_array(&gvb, "HLR",
+                    &act->b.hlr);
+            }
+            break;
+        case NFC_TECHNOLOGY_F:
+        case NFC_TECHNOLOGY_UNKNOWN:
+            break;
+        }
+    }
+    return g_variant_builder_end(&gvb);
+}
+
+/* GetAll2 */
+
+static
+gboolean
+dbus_service_isodep_handle_get_all2(
+    OrgSailfishosNfcIsoDep* iface,
+    GDBusMethodInvocation* call,
+    DBusServiceIsoDep* self)
+{
+    NfcTagType4* t4 = self->t4;
+    const NfcParamIsoDep *act = t4->iso_dep;
+
+    org_sailfishos_nfc_iso_dep_complete_get_all2(iface, call,
+        NFC_DBUS_ISODEP_INTERFACE_VERSION,
+        dbus_service_isodep_get_act_parameters(t4, act));
+    return TRUE;
+}
+
+/* GetActivationParameters */
+
+static
+gboolean
+dbus_service_isodep_handle_get_act_parameters(
+    OrgSailfishosNfcIsoDep* iface,
+    GDBusMethodInvocation* call,
+    DBusServiceIsoDep* self)
+{
+    NfcTagType4* t4 = self->t4;
+    const NfcParamIsoDep *act = t4->iso_dep;
+
+    org_sailfishos_nfc_iso_dep_complete_get_activation_parameters(iface,
+        call, dbus_service_isodep_get_act_parameters(t4, act));
+    return TRUE;
+}
+
 /*==========================================================================*
  * Interface
  *==========================================================================*/
@@ -246,6 +359,12 @@ dbus_service_isodep_new(
     self->call_id[CALL_TRANSMIT] =
         g_signal_connect(self->iface, "handle-transmit",
         G_CALLBACK(dbus_service_isodep_handle_transmit), self);
+    self->call_id[CALL_GET_ALL2] =
+        g_signal_connect(self->iface, "handle-get-all2",
+        G_CALLBACK(dbus_service_isodep_handle_get_all2), self);
+    self->call_id[CALL_GET_ACTIVATION_PARAMETERS] =
+        g_signal_connect(self->iface, "handle-get-activation-parameters",
+        G_CALLBACK(dbus_service_isodep_handle_get_act_parameters), self);
 
     if (g_dbus_interface_skeleton_export(G_DBUS_INTERFACE_SKELETON
         (self->iface), connection, path, &error)) {
