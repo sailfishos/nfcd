3279a15413bedac7372c697ed76298a938e24242
Slava Monich <slava.monich@jolla.com>
2020-08-07 13:49:25 +0300

    Version 1.0.37

--- core/include/nfc_version.h
+++ core/include/nfc_version.h
@@ -35,7 +35,7 @@
 
 #define NFC_VERSION_MAJOR 1
 #define NFC_VERSION_MINOR 0
-#define NFC_VERSION_NANO  36
+#define NFC_VERSION_NANO  37
 
 #define NFC_VERSION_WORD(v1,v2,v3) \
     ((((v1) & 0x7f) << 24) | \
--- rpm/nfcd.spec
+++ rpm/nfcd.spec
@@ -1,5 +1,5 @@
 Name: nfcd
-Version: 1.0.36
+Version: 1.0.37
 Release: 0
 Summary: NFC daemon
 License: BSD
