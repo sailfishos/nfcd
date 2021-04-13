3e535e92084dba033c49b9588e4755497cc509ae
Slava Monich <slava.monich@jolla.com>
2021-04-13 19:45:32 +0300

    [plugins] Housekeeping

--- plugins/settings/plugin.h
+++ plugins/settings/plugin.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2018 Jolla Ltd.
- * Copyright (C) 2018 Slava Monich <slava.monich@jolla.com>
+ * Copyright (C) 2018-2021 Jolla Ltd.
+ * Copyright (C) 2018-2021 Slava Monich <slava.monich@jolla.com>
  *
  * You may use this file under the terms of BSD license as follows:
  *
@@ -14,8 +14,8 @@
  *      notice, this list of conditions and the following disclaimer in the
  *      documentation and/or other materials provided with the distribution.
  *   3. Neither the names of the copyright holders nor the names of its
- *      contributors may be used to endorse or promote products derived from
- *      this software without specific prior written permission.
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -30,14 +30,14 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef SAILFISH_PLUGIN_H
-#define SAILFISH_PLUGIN_H
+#ifndef SETTINGS_PLUGIN_H
+#define SETTINGS_PLUGIN_H
 
 #include <nfc_plugin.h>
 
 NFC_PLUGIN_DECLARE(settings)
 
-#endif /* SAILFISH_PLUGIN_H */
+#endif /* SETTINGS_PLUGIN_H */
 
 /*
  * Local Variables:
