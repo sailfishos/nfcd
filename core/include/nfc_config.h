fe3a663868f44d3318079f8f9df778198859fa74
Slava Monich <slava@monich.com>
2023-09-24 14:12:40 +0300

    [core] Explicitly mark NfcConfigurable API as exported

--- core/include/nfc_config.h
+++ core/include/nfc_config.h
@@ -1,33 +1,36 @@
 /*
+ * Copyright (C) 2022-2023 Slava Monich <slava@monich.com>
  * Copyright (C) 2022 Jolla Ltd.
- * Copyright (C) 2022 Slava Monich <slava.monich@jolla.com>
  *
- * You may use this file under the terms of BSD license as follows:
+ * You may use this file under the terms of the BSD license as follows:
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *   1. Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in the
- *      documentation and/or other materials provided with the distribution.
- *   3. Neither the names of the copyright holders nor the names of its
- *      contributors may be used to endorse or promote products derived
- *      from this software without specific prior written permission.
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer
+ *     in the documentation and/or other materials provided with the
+ *     distribution.
+ *  3. Neither the names of the copyright holders nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING
+ * IN ANY WAY OUT OF THE USE OR INABILITY TO USE THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * any official policies, either expressed or implied.
  */
 
 #ifndef NFC_CONFIG_H
@@ -60,7 +63,7 @@ G_BEGIN_DECLS
  * as the default value.
  */
 
-GType nfc_configurable_get_type(void);
+GType nfc_configurable_get_type(void) NFCD_EXPORT;
 #define NFC_TYPE_CONFIGURABLE (nfc_configurable_get_type())
 #define NFC_CONFIGURABLE(obj) G_TYPE_CHECK_INSTANCE_CAST(obj, \
         NFC_TYPE_CONFIGURABLE, NfcConfigurable)
@@ -100,30 +103,35 @@ struct nfc_configurable_interface {
 
 const char* const*
 nfc_config_get_keys(
-    NfcConfigurable* conf);
+    NfcConfigurable* config)
+    NFCD_EXPORT;
 
 GVariant*
 nfc_config_get_value(
     NfcConfigurable* config,
-    const char* key);
+    const char* key)
+    NFCD_EXPORT;
 
 gboolean
 nfc_config_set_value(
     NfcConfigurable* config,
     const char* key,
-    GVariant* value);
+    GVariant* value)
+    NFCD_EXPORT;
 
 gulong
 nfc_config_add_change_handler(
     NfcConfigurable* config,
     const char* key,
     NfcConfigChangeFunc func,
-    void* user_data);
+    void* user_data)
+    NFCD_EXPORT;
 
 void
 nfc_config_remove_handler(
     NfcConfigurable* config,
-    gulong id);
+    gulong id)
+    NFCD_EXPORT;
 
 G_END_DECLS
 
